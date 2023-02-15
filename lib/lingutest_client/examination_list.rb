0263c58205eb9bbd449a4fe137227241a3aed3f4
Andrew <andrew.shyika@gmail.com>
2023-02-15 22:36:34 +0200

    Add pagination to examination list

--- lib/lingutest_client/examination_list.rb
+++ lib/lingutest_client/examination_list.rb
@@ -6,5 +6,11 @@ module LingutestClient
     delegate ::LingutestClient::API::Operations::List::DELEGATED_METHODS => :examinations
 
     attribute :examinations, Types::Array.of(Examination)
+    attribute :page, Types::Integer
+    attribute :per_page, Types::Integer
+    attribute :current_page, Types::Integer
+    attribute :limit_value, Types::Integer
+    attribute :total_pages, Types::Integer
+    attribute :total_count, Types::Integer
   end
 end
